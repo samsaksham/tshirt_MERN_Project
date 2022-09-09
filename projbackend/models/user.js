80b45b2ce89376b57e33c72212df89fcdfc598c2
samsaksham <rajput.saksham29@gmail.com>
2022-09-09 22:24:22 +0530

    Initial commit

---
+++ projbackend/models/user.js
@@ -0,0 +1,83 @@
+var mongoose = require("mongoose");
+const crypto = require("crypto");
+const uuidv1 = require("uuid/v1");
+
+
+var userSchema = new mongoose.Schema({
+    name: {
+        type :String,
+        required:true,
+        maxlength :32,
+        trim:32
+    },
+    lastname: {
+        type :String,
+        maxlength :32,
+        trim:32
+    },
+    email :{
+        type :String,
+        maxlength :32,
+        required:true,
+        unique :true,
+        trim:32
+    },
+    userinfo:{
+        type:String,
+        trim:true
+    },
+
+    encry_password : {
+        type :String,
+        required :true
+    },
+
+    salt : String,
+
+    role: {
+        type:Number,
+        default:0
+    },
+
+    purchases : {
+        type: Array,
+        default:[]
+    }
+},{ timestamps: true });
+
+userSchema
+.virtual("password")
+.set(function(password){
+  this._password = password;
+  this.salt = uuidv1();
+  this.encry_password = this.securePassword(password);
+})
+.get(function(){
+  return this._password;
+});
+
+
+
+userSchema.methods = {
+    autheticate: function(plainpassword){
+        return this.securePassword(plainpassword) === this.encry_password;
+      },
+
+    securePassword: function(plainpassword){
+        if(!plainpassword) return "";
+
+        try {
+            return crypto
+            .createHmac("sha256", this.salt)
+            .update(plainpassword)
+            .digest("hex");
+            
+        } catch (err) {
+            return ""
+            
+        }
+    }
+}
+
+module.exports = mongoose.model("User",userSchema)
+
